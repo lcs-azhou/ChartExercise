5f3bc4c3752b7ce0cc1e8c7b5c222483de55bffd
Ansheng Zhou <azhou@lcs.on.ca>
2024-11-04 17:12:15 -0500

    Initial Commit

---
+++ ChartExercise/ChartExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartExerciseApp.swift
+//  ChartExercise
+//
+//  Created by Ansheng Zhou on 2024-11-04.
+//
+
+import SwiftUI
+
+@main
+struct ChartExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChartExerciseTests/ChartExerciseTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChartExerciseTests.swift
+//  ChartExerciseTests
+//
+//  Created by Ansheng Zhou on 2024-11-04.
+//
+
+import Testing
+@testable import ChartExercise
+
+struct ChartExerciseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
